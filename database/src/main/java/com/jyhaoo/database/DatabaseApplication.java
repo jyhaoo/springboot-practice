2bbca7ab4b2cfafe08a47b3a69883eb6b6030c5c
Jason Hao <jyhao1998@gmail.com>
2024-02-05 16:42:03 -0800

    feat: add connection to postgress db

--- database/src/main/java/com/jyhaoo/database/DatabaseApplication.java
+++ database/src/main/java/com/jyhaoo/database/DatabaseApplication.java
@@ -1,16 +1,30 @@
 package com.jyhaoo.database;
 
 import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class DatabaseApplication {
+@Log
+public class DatabaseApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+
+	public DatabaseApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
 }
