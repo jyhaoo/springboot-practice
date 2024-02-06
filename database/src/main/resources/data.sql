c3060a1dd1eac0a726541185b7e36c05986aa624
Jason Hao <jyhao1998@gmail.com>
2024-02-05 21:41:08 -0800

    feat: add dummy sql data

---
+++ database/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'Widget A', 'Testing'),
+(2, 'Widget B', 'Helping'),
+(3, 'Widget C', 'Designing'),
+(4, 'Widget D', 'More Testing'),
+(5, 'Widget E', 'Push to Production');
