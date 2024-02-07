5d5890f42d9343f0df1d6f172268712577dbc512
Jason Hao <jyhao1998@gmail.com>
2024-02-06 18:01:31 -0800

    Initial commit

---
+++ jdbc-template/src/main/java/com/jyhaoo/jdbctemplate/JdbcTemplateApplication.java
@@ -0,0 +1,15 @@
+package com.jyhaoo.jdbctemplate;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class JdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplateApplication.class, args);
+	}
+
+}
