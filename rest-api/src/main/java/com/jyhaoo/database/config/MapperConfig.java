5641a2c96189c522a06f50edbdc8473efb4172c4
Jason Hao <jyhao1998@gmail.com>
2024-02-17 12:03:48 -0800

    feat: add book and author mapper

--- rest-api/src/main/java/com/jyhaoo/database/config/MapperConfig.java
+++ rest-api/src/main/java/com/jyhaoo/database/config/MapperConfig.java
@@ -1,6 +1,8 @@
 package com.jyhaoo.database.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,8 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         return new ModelMapper();
     }
 }
